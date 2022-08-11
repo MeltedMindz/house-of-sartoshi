19f5668c904e24af0b5c8ae2d9cc92080f862e8f
thebadcc <95499220+thebadcc@users.noreply.github.com>
2022-08-11 16:18:47 -0400

    Update script.js

--- cryptovoxels/scripts/script.js
+++ cryptovoxels/scripts/script.js
@@ -7,3 +7,18 @@ feature.on('click',e=>{
   x.scale.set(0.01,0.01,0.01)
 },5000)
 })
+
+// token gate
+
+feature.on('click',e=>{
+let mfer = e.player.wallet
+let url = 'https://api.etherscan.io/api?module=account&action=tokenbalance&contractaddress=0xFf8b31bAA28a638AFf795FE36E73200303157b7c&address='  + mfer + '&tag=latest&apikey=DKZA18WTG7ZCPMRJ52K74HQTNIISQ4AE6B'
+fetch(url)
+  .then(r => r.json())
+  .then( r=> {
+   if (r.result > 0)    {
+     e.player.teleportTo('E@73E,445S,1F')
+    } else {
+    }
+  })
+})
